a7345bcdefabd5bb4e6c155d7aecc263e51e00ea
lazzarus <ss22072000@gmail.com>
2021-10-08 13:14:54 +0530

    Added genre sort functionality

--- genre_sort.py
+++ genre_sort.py
@@ -1,4 +1,6 @@
 import os
+import os.path
+import shutil
 import csv
 
 TRACK_METADATA_PATH = '../../Datasets/fma_metadata/tracks_new.csv'
@@ -7,6 +9,8 @@ TRACK_ID_COLUMN = 0
 TRACK_GENRE_COLUMN = 42
 GENRE_ID_COLUMN = 0
 GENRE_NAME_COLUMN = 3
+FMA_PATH = '../../Datasets/fma_medium/'
+FMA_SORTED_PATH = '../../Datasets/fma_genre'
 
 genres = {}
 
@@ -27,9 +31,45 @@ for row in reader:
     split_string = row[TRACK_GENRE_COLUMN][1:-1].strip().split(",")
     track_genre_top = split_string[0]
     if(track_genre_top in genres.keys()):
-        track_genres[row[TRACK_ID_COLUMN]] = genres[track_genre_top]
+        track_genres[row[TRACK_ID_COLUMN].zfill(6)] = genres[track_genre_top]
 
-print(track_genres['2'])
+def searchFile(fileName):
+    for root, dirs, files in os.walk(FMA_PATH):
+        print('Looking in:',root)
+        for Files in files:
+            try:
+                found = Files.find(fileName)
+                if found != -1:
+                   return f"{root}/{fileName}"
+            except:
+                return None
+
+def make_and_move(track_genres):
+    print("Creating Genre Folders...")
+    os.chdir(FMA_SORTED_PATH)
+    fileli = os.listdir()
+    for genre in genres:
+        genre = genres[genre]
+        os.mkdir(genre)
+    print("Successfully created folders!")
+    for track in track_genres:
+        print(f"Finding {track}.mp3")
+        filePath = searchFile(track)
+        if(filePath):
+            filePath = filePath+'.mp3'
+            print(f"Found {track}.mp3 in {filePath}")
+            print(f"Now Copying {track}.mp3 ... to {FMA_SORTED_PATH}/{track_genres[track]}")
+            genre_path = f"{FMA_SORTED_PATH}/{track_genres[track]}/"
+            shutil.copy(filePath, genre_path)
+            print(f"Successfully Copied {track}.mp3")
+            
+        else:
+            print(f"Unable to find {track}")
+
+    
+
+make_and_move(track_genres)
+
 
 
      
